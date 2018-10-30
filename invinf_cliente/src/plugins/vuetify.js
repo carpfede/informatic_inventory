7ebe66ed91a52efbdc9ebb29104fb8a4af6aba89
carpfede <fedefiasare@gmail.com>
2018-10-30 02:59:15 -0300

    plugin vuetify

---
+++ invinf_cliente/src/plugins/vuetify.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify, {
+  iconfont: 'fa'
+})

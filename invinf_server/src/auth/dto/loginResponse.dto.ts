4081b24461db3d5d952605c7ab4ddf391ed826e4
Federico Gabriel Fia Sare <carpfede>
2018-11-24 18:03:15 -0300

    remove auth obsolete

--- invinf_server/src/auth/dto/loginResponse.dto.ts
+++
@@ -1,9 +0,0 @@
-export class LoginResponse {
-    token: any;
-    errors: [];
-
-    public constructor(token = null, errors = null) {
-        this.token = token;
-        this.errors = errors;
-    }
-}
--- invinf_server/src/auth/dto/loginUser.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiModelProperty } from '@nestjs/swagger';
-
-export class LoginUserDto {
-  @ApiModelProperty()
-  readonly userName: string;
-
-  @ApiModelProperty()
-  readonly password: string;
-}
